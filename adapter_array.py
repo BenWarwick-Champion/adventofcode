33e76b635aa19be704e030d89c06fef4421ec49e
Ben Warwick-Champion <ben.w-c@sky.com>
2020-12-10 18:18:44 +0000

    2020 Day 10 part 2 solution

--- adapter_array.py
+++ adapter_array.py
@@ -1,6 +1,20 @@
 # Advent of Code 2020
 # Day 10: Adapter Array
 
+# Returns permutations from adapter[i]
+def permutations_from(i):
+    if i == len(adapters) - 1:
+        return 1 # base case
+    if i in memory:
+        return memory[i] # avoid recalculation
+
+    ans = 0
+    for j in range(i+1, len(adapters)):
+        if adapters[j] - adapters[i] <= 3:
+            ans += permutations_from(j)
+    memory[i] = ans
+    return ans
+
 if __name__ == "__main__":
     with open("Data/day10.txt", "r") as f:
         input_data = [line.rstrip('\n') for line in f.readlines()]
@@ -12,5 +26,28 @@ if __name__ == "__main__":
     jolt_diff = [adapters[i] - adapters[i-1] for i in range(1, len(adapters))]
 
     part1 = jolt_diff.count(1) * jolt_diff.count(3)
-    print(jolt_diff.count(1), jolt_diff.count(3))
-    print(part1)
+    print("Part 1 solution:", part1)
+
+    memory = {}
+    part2 = permutations_from(0)
+    print("Part 2 solution:", part2)
+
+    # counts = []
+    # i = 0
+    # while i < len(adapters[:-2]):
+    #     j = i + 1
+    #     count = 0
+    #     while adapters[j] - adapters[i] <= 3 and j != len(adapters[:-1]):
+    #         count = j - i
+    #         j += 1
+    #     if count == 3:
+    #         counts.append(4)
+    #     else:
+    #         counts.append(count)
+    #     i = j - 1
+
+    # total = 1
+    # for num in counts:
+    #     total *= num
+
+    # print(total) #8, 19208
