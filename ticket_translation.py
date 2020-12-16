4f652e4b26ff8db77da3d787d54a30d0f913a8ce
Ben Warwick-Champion <ben.w-c@sky.com>
2020-12-16 14:10:53 +0000

    2020 Day 16 part 2

--- ticket_translation.py
+++ ticket_translation.py
@@ -1,6 +1,7 @@
 # Advent of Code 2020
 # Day 16: Ticket Translation
 
+import copy
 
 if __name__ == "__main__":
     with open("Data/day16.txt", "r") as f:
@@ -43,3 +44,53 @@ if __name__ == "__main__":
             
     ticket_error_rate = sum(invalid_values)
     print("Part 1 solution:", ticket_error_rate) # 26009
+
+    valid_tickets = []
+    for ticket in tickets:
+        is_valid = True
+        for num in ticket:
+            if num in invalid_values:
+                is_valid = False
+                break
+        if is_valid:
+            valid_tickets.append(ticket)
+
+    # fields holds a list of list
+    fields = []
+    for i in range(len(tickets[0])):
+        fields.append([ticket[i] for ticket in valid_tickets])
+
+    # remaining restrictions
+    r_res = copy.deepcopy(restrictions)
+    correct_field = {}
+    for f in fields:
+        for r in r_res:
+            l_b = r_res[r][0][0]
+            mlb = r_res[r][0][1]
+            mub = r_res[r][1][0]
+            u_b = r_res[r][1][1]
+
+            if all((l_b <= n <= mlb or mub <= n <= u_b) for n in f):
+                correct_field.setdefault(r, []).append(fields.index(f))
+
+    # correct_field:
+    # 'departure time' : [0, 1, 2, 3, 4, 5, 6, 8, 10, 12, 13, 14, 15, 16, 17, 18]
+    # 'row' : [13, 15, 17]
+    # 'seat' : [13, 17]
+    # 'type' : [13]
+
+    # c_f is correct_fields but with only 1 value
+    c_f = dict()
+    for f1 in correct_field:
+        for f2 in correct_field:
+            chk = set(correct_field[f1]).difference(set(correct_field[f2]))
+            if len(chk) == 1:
+                c_f[f1] = chk.pop()
+
+    # Multiply the 6 departure values together
+    total = 1
+    for f in c_f:
+        if 'departure' in f:
+            total *= my_ticket[c_f[f]]
+
+    print("Part 2 solution:", total) # 589685618167
