f5ae2a84f46aa012759c7c34050c862af942947e
Ben Warwick-Champion <ben.w-c@sky.com>
2020-12-18 12:12:00 +0000

    2020 Day 18 part 2 solution

--- operation_order.py
+++ operation_order.py
@@ -27,6 +27,23 @@ def evaluate(string):
     return ans
 
 
+def eval_backwards(string):
+    if '(' in string:
+        eq = string[1:-1].split()
+    else:
+        eq = string.split()
+
+    while eq.count('+') > 0:
+        ind = eq.index('+')
+        res = int(eq[ind-1]) + int(eq[ind+1])
+        for _ in range(3):
+            eq.pop(ind-1)
+        eq.insert(ind-1, str(res))
+    
+    ans = eval(''.join(eq))
+    return ans
+
+
 if __name__ == "__main__":
     with open("Data/day18.txt", "r") as f:
         hw = [line.strip() for line in f.readlines()]
@@ -39,10 +56,24 @@ if __name__ == "__main__":
             start = find_nth(lastline, '(', ob)
             end = lastline.find(')', start) + 1
             result = evaluate(lastline[start:end])
-            newline = lastline.replace(lastline[start:end], str(result)) # lastline[:start] + str(result) + lastline[end:]
+            newline = lastline.replace(lastline[start:end], str(result))
             lastline = newline
             ob -= 1
-
         hw_sum += evaluate(lastline)
 
     print("Part 1 solution:", hw_sum) # 21022630974613
+
+    hw_sum = 0
+    for line in hw:
+        ob = line.count('(')
+        lastline = line
+        while ob > 0:
+            start = find_nth(lastline, '(', ob)
+            end = lastline.find(')', start) + 1
+            result = eval_backwards(lastline[start:end])
+            newline = lastline.replace(lastline[start:end], str(result))
+            lastline = newline
+            ob -= 1
+        hw_sum += eval_backwards(lastline)
+
+    print("Part 2 solution:", hw_sum) # 169899524778212
