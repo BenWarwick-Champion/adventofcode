45b002c67e604a4ac5001d3e958b1d7eb54923d6
Ben Warwick-Champion <ben.w-c@sky.com>
2020-12-30 16:05:25 +0000

    Able to build first row of puzzle

--- jurassic_jigsaw.py
+++ jurassic_jigsaw.py
@@ -1,34 +1,81 @@
 # Advent of Code 2020
 # Day 20: Jurassic Jigsaw
 
-import numpy as np
+from pprint import pprint
+from copy import deepcopy
 
 def uid_edge(edge):
     r_edge = [x for x in reversed(edge)]
     uid = min(edge, r_edge)
     return uid
 
-
-def find_edges(piece):
+def find_edges(piece, unique=False):
     edges = []
     top_edge = piece[0]
     bottom_edge = piece[-1]
     right_edge = [piece[i][-1] for i in range(len(piece))]
     left_edge = [piece[i][0] for i in range(len(piece))]
 
-    edges.append(uid_edge(top_edge))
-    edges.append(uid_edge(right_edge))
-    edges.append(uid_edge(bottom_edge))
-    edges.append(uid_edge(left_edge))
+    if unique:
+        edges.append(uid_edge(top_edge))
+        edges.append(uid_edge(right_edge))
+        edges.append(uid_edge(bottom_edge))
+        edges.append(uid_edge(left_edge))
+    else:
+        edges.append(top_edge)
+        edges.append(right_edge)
+        edges.append(bottom_edge)
+        edges.append(left_edge)
     return edges
 
-
 def flip(piece):
-    pass
+    flipped = []
+    for line in piece[::-1]:
+        flipped.append(line)
+    return flipped
 
+def rotate_right(piece):
+    l = len(piece)-1
+    r = 0
+    while r < l:
+        c = r
+        lr = l - r
+        while c < lr:
+            lc = l - c
+            mem = piece[r][c]
+            piece[r][c] = piece[lc][r]
+            piece[lc][r] = piece[lr][lc]
+            piece[lr][lc] = piece[c][lr]
+            piece[c][lr] = mem
+            c += 1
+        r += 1
+    return piece
 
-def rotate(piece):
-    pass
+def rotate_left(piece):
+    l = len(piece)-1
+    r = 0
+    while r < l:
+        c = r
+        lr = l - r
+        while c < lr:
+            lc = l - c
+            mem = piece[r][c]
+            piece[r][c] = piece[c][lr]
+            piece[c][lr] = piece[lr][lc]
+            piece[lr][lc] = piece[lc][r]
+            piece[lc][r] = mem
+            c += 1
+        r += 1
+    return piece
+
+def trim(piece):
+    trimmed = []
+    for ind, line in enumerate(piece):
+        if ind == 0 or ind == len(piece):
+            continue
+        else:
+            trimmed.append(line[1:-1])
+    return trimmed
 
 
 if __name__ == "__main__":
@@ -40,14 +87,16 @@ if __name__ == "__main__":
         if 'Tile' in line:
             pieces[line[5:-1]] = [list(l) for l in data[ind+1:ind+11]]
     edges = {}
+    unique_edges = {}
     for piece in pieces:
         edges[piece] = find_edges(pieces[piece])
+        unique_edges[piece] = find_edges(pieces[piece], True)
 
     # Create a list of uids for every edge
     uids = []
-    for piece in edges:
+    for piece in unique_edges:
         for i in range(4):
-            uids.append(edges[piece][i]) # uid
+            uids.append(unique_edges[piece][i]) # uid
     
     # 12x12 expects 48 outside edges with no match
     outside_edges = []
@@ -59,15 +108,63 @@ if __name__ == "__main__":
     # Corner pieces have two outside edges
     corners = []
     d_corners = {}
-    for piece in edges:
+    for piece in unique_edges:
         count = 0
-        count = sum([1 for edge in outside_edges if edge in edges[piece]])
+        count = sum([1 for edge in outside_edges if edge in unique_edges[piece]])
         if count > 1:
             corners.append(piece)
-            d_corners[piece] = [edge for edge in outside_edges if edge in edges[piece]]
+            d_corners[piece] = [edge for edge in outside_edges if edge in unique_edges[piece]]
 
     print(corners) # '1283', '1511', '1619', '1901'
     total = 1
     for corner in corners:
         total *= int(corner)
     print("Part 1 solution:", total)
+
+    r_pieces = list(pieces.keys()) # deepcopy(pieces)
+    top_left_id = '1283'
+    top_left = flip(rotate_left(pieces[top_left_id])) # get this by looking at the data
+    pieces[top_left_id] = top_left
+    puzzle_row = [top_left_id] # Just start with a top-left corner
+    while len(puzzle_row) < 12:
+        curr = puzzle_row[-1]
+        r_pieces.remove(curr)
+        modified = False
+        curr_edges = find_edges(pieces[curr])
+
+        for piece in r_pieces:
+            if modified:
+                break
+            chk_edges = find_edges(pieces[piece])
+            for edge in chk_edges:
+                # if the right edge of the current piece is the same as the searched piece
+                # add that piece to the row (in the right orientation) 
+                if curr_edges[1] == edge:
+                    i = chk_edges.index(edge)
+                    if i == 0:
+                        pieces[piece] = flip(rotate_left(pieces[piece]))
+                    elif i == 1:
+                        pieces[piece] = flip(rotate_left(rotate_left(pieces[piece])))
+                    elif i == 2:
+                        pieces[piece] = rotate_right(pieces[piece])
+                    else:
+                        pass
+                    puzzle_row.append(piece)
+                    break
+
+            for edge in chk_edges:
+                r_edge = [i for i in edge[::-1]]
+                if curr_edges[1] == r_edge:
+                    i = chk_edges.index(edge)
+                    if i == 0:
+                        pieces[piece] = rotate_left(pieces[piece])
+                    elif i == 1:
+                        pieces[piece] = rotate_left(rotate_left(pieces[piece]))
+                    elif i == 2:
+                        pieces[piece] = flip(rotate_right(pieces[piece]))
+                    else:
+                        pieces[piece] = flip(pieces[piece])
+                    puzzle_row.append(piece)
+                    break
+
+    print(puzzle_row)
