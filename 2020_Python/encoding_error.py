7c3739f2cf6d2973acc641fe90bfbf6085dd9529
Ben Warwick-Champion <ben.w-c@sky.com>
2021-01-07 17:44:15 +0000

    Minor refactor and corrected display of part 2 solution

--- 2020_Python/encoding_error.py
+++ 2020_Python/encoding_error.py
@@ -7,25 +7,17 @@ def sliding_window(data, preamble):
         if not check_sums(window, data[ind-1]):
             return data[ind-1], ind - 1
 
-
 def sliding_window_part2(data, preamble, ans):
     for ind in range(preamble + 1, len(data)):
         window = data[ind - preamble - 1 : ind - 1]
-        if sum_all(window, ans):
+        if sum(window) == ans:
             return min(window) + max(window)
-
 
 def check_sums(window, num):
     for num1 in window:
         for num2 in window:
             if (num1 != num2) and (num == num1 + num2):
                 return True
-    return False
-
-
-def sum_all(window, ans):
-    if ans == sum(window):
-        return True
     return False
 
 
@@ -38,4 +30,4 @@ if __name__ == "__main__":
 
     part2_data = input_data[:index]
     part2 = [x for x in (sliding_window_part2(part2_data, i, answer) for i in range(len(part2_data))) if x is not None]
-    print("The solution to Part 2 is:", part2) # 76688505
+    print("The solution to Part 2 is:", *part2) # 76688505
