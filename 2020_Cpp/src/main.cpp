6ccd04b0e3298ed36ef9ace9de40cc7318e231cd
Ben Warwick-Champion <ben.w-c@sky.com>
2021-01-13 19:55:51 +0000

    Added performance timing and made load_input error messages more useful

--- 2020_Cpp/src/main.cpp
+++ 2020_Cpp/src/main.cpp
@@ -1,4 +1,5 @@
 #include <fstream>
+#include <chrono>
 #include "advent2020.h"
 
 static const std::vector<advent_t> advent2020 = {
@@ -9,6 +10,11 @@ static input_t load_input(const std::string &filename);
 static void free_input(input_t &input);
 
 int main(void) {
+
+    double total_time = 0;
+
+    printf("          Time         Part 1           Part 2\n");
+	printf("========================================================\n");
     for (int day = 1; day <= advent2020.size(); day++) {
         auto &A = advent2020[day - 1];
         if (!A.fn) continue;
@@ -16,13 +22,23 @@ int main(void) {
         std::string filename = "../Data/day" + std::to_string(day) + ".txt";
 
         auto input = load_input(filename);
-        auto output = A.fn(input);
 
-        printf("Day %02d: %-16s %-16s\n",
-                day, 
+        auto t0 = std::chrono::steady_clock::now();
+        auto output = A.fn(input);
+        auto elapsed = std::chrono::steady_clock::now() - t0;
+
+        double t = 1e-3 * std::chrono::duration_cast<std::chrono::nanoseconds>(elapsed).count();
+        total_time += t;
+
+        printf("Day %02d: %7.f μs     %-16s %-16s\n",
+                day,
+                t, 
                 output.answer[0].c_str(),
                 output.answer[1].c_str());
     }
+    printf("========================================================\n");
+	printf("Total:  %7.f μs\n", total_time);
+
     return 0;
 }
 
@@ -37,16 +53,15 @@ input_t load_input(const std::string &filename) {
         char* buffer = new char[in.len];
         file.read(buffer, in.len);
 
-        if(file)
-        {
-            std::cout << "All chars read successfully" << std::endl;
-        }
-        else
+        if(!file)
         {
             std::cout << "Error: Only " << file.gcount() << " could be read";
         }
+        
         file.close();
         in.str = buffer;
+    } else {
+        std::cout << "Error reading file. You may need to run 'cd build'" << std::endl;
     }
     return in;
 }
