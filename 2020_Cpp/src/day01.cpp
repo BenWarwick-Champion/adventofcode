b4db0a74f530c1cc5886b148a25c1966e4e60a8b
Ben Warwick-Champion <ben.w-c@sky.com>
2021-01-13 14:57:58 +0000

    Added Day 1 Part 1 solution

--- 2020_Cpp/src/day01.cpp
+++ 2020_Cpp/src/day01.cpp
@@ -2,5 +2,23 @@
 
 output_t day01(input_t in) {
     int part1 = 0, part2 = 0;
+
+    auto numbers = parse::str_to_vec(in);
+    display::print_vec(numbers);
+
+    std::unordered_set<int> set;
+
+    for (int i = 0; i < numbers.size(); i++) {
+        set.insert(numbers[i]);
+    }
+
+    for (int i = 0; i < numbers.size(); i++) {
+        int res = 2020 - numbers[i];
+        if (set.find(res) != set.end()) {
+            part1 = numbers[i] * res;
+            break;
+        }
+    }
+
     return { part1, part2 };
 }
