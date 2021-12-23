862bf4f938eb5513074dd64ee2571ae5d37d7a3a
Ben Warwick-Champion <ben.warwick-champion@automation-consultants.com>
2021-12-23 17:35:38 +0000

    :snake: Solved Day 23 by hand...

--- 2021/python-solutions/src/day24.py
+++ 2021/python-solutions/src/day24.py
@@ -1,14 +1,11 @@
 # Advent of Code
-# Day 23
+# Day 24
 
-# A: 1, B: 10, C: 100, D: 1000
 
-# Solved the puzzle by hand...
 
 if __name__ == '__main__':
-  with open('input/day23.txt', 'r') as f:
+  with open('input/day24_test.txt', 'r') as f:
     data = f.read()
 
   print('---- Part One ----')
   print('---- Part Two ----')
-
