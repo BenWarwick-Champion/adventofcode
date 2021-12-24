c8867dd7d04b004a20b013b6e3910c7cd0201026
Ben Warwick-Champion <ben.warwick-champion@automation-consultants.com>
2021-12-24 14:07:28 +0000

    :snake: Christmas Eve solutions

--- 2021/python-solutions/src/day24.py
+++ 2021/python-solutions/src/day24.py
@@ -1,11 +1,31 @@
 # Advent of Code
 # Day 24
 
-
+def solve(blocks):
+  model_max = [0] * 14
+  model_min = [0] * 14
+  stack = []
+  for i, block in enumerate(blocks):
+    if block[3] == 'div z 1':
+      stack.append((i, int(block[14].split(' ')[-1]))) # add y <val>
+    elif block[3] == 'div z 26':
+      j, x = stack.pop()
+      diff = x + int(block[4].split(' ')[-1]) # add x <-val>
+      if diff < 0:
+        i, j, diff = j, i, -diff
+      model_max[i] = 9
+      model_max[j] = 9 - diff
+      model_min[i] = 1 + diff
+      model_min[j] = 1
+  return model_max, model_min
 
 if __name__ == '__main__':
-  with open('input/day24_test.txt', 'r') as f:
-    data = f.read()
+  with open('input/day24.txt', 'r') as f:
+    blocks = [block.split('\n') for block in f.read().split('inp w\n')[1:]]
+  
+  part1, part2 = solve(blocks)
 
   print('---- Part One ----')
+  print(''.join(str(n) for n in part1))
   print('---- Part Two ----')
+  print(''.join(str(n) for n in part2))
