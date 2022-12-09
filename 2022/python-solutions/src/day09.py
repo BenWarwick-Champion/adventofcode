b9f16b95d81a4a8504a68747f11900b4b89f0b21
Ben Warwick-Champion <ben.w-c@sky.com>
2022-12-09 22:19:02 +0000

    :sparkles: Added d09 part 2

--- 2022/python-solutions/src/day09.py
+++ 2022/python-solutions/src/day09.py
@@ -2,49 +2,49 @@
 # Day 09
 
 direction_map = {
-    'U': 1,
-    'D': -1,
-    'L': -1j,
-    'R': 1j,
+    'R': 1,
+    'L': -1,
+    'D': -1j,
+    'U': 1j,
 }
 
 
+def sign(num):
+    return (num > 0) - (num < 0)
+
+
+def complex_sign(c_num):
+    return sign(c_num.real) + sign(c_num.imag) * 1j
+
+
 def find_visited(instructions):
-    t_pos = complex(0, 0)
-    h_pos = complex(0, 0)
-    visited = set()
+    rope = [0 for _ in range(10)]
+    p1_visited = set()
+    p2_visited = set()
+
     for instruction in instructions:
         direction, distance = instruction.split()
-        # print(instruction.strip())
-        for step in range(int(distance)):
-            h_pos += direction_map[direction]
-            # visited.add(t_pos)
 
-            diff = h_pos - t_pos
-            # print(f'h_pos: {h_pos}, t_pos: {t_pos}, diff: {diff}')
-            # print(f'abs diff.imag: {abs(diff.imag)}')
-            if abs(diff.real) >= 2 or abs(diff.imag) >= 2:
-                # We need to catch up
-                if abs(diff.real) == 2 and diff.imag == 0:
-                    t_pos += diff.real // 2
-                elif abs(diff.real) == 2:
-                    t_pos += complex(diff.real // 2, diff.imag)
+        for _ in range(int(distance)):
+            rope[0] += direction_map[direction]
+            for i in range(1, 10):
+                diff = rope[i-1] - rope[i]
 
-                if abs(diff.imag) == 2 and diff.real == 0:
-                    t_pos += complex(0, diff.imag // 2)
-                elif abs(diff.imag) == 2:
-                    t_pos += complex(diff.real, diff.imag // 2)
-            visited.add(t_pos)
+                if abs(diff) >= 2:
+                    rope[i] += complex_sign(diff)
 
-    return visited
+            p1_visited.add(rope[1])
+            p2_visited.add(rope[-1])
+    return p1_visited, p2_visited
 
 
 if __name__ == "__main__":
     with open("input/day09.txt") as f:
         data = f.readlines()
 
+    p1, p2 = find_visited(data)
     print('---- Part One ----')
-    print(len(find_visited(data)))  # 6338 too low
+    print(len(p1))  # 6339
 
     print('---- Part Two ----')
-    print()
+    print(len(p2))  # 2541
