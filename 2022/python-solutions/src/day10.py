93591f336e97b55e8e2c669782bef346047be135
Ben Warwick-Champion <ben.w-c@sky.com>
2022-12-11 09:16:33 +0000

    :sparkles: Day 10 p2

--- 2022/python-solutions/src/day10.py
+++ 2022/python-solutions/src/day10.py
@@ -27,14 +27,39 @@ def execute(instructions):
     return x_map
 
 
+def draw_CRT(x_map):
+    screen = []
+    for clock in x_map:
+        pos = clock % 40
+        num = x_map[clock]
+        sprite = [num, num+1, num+2]
+        draw = False
+        for val in sprite:
+            if pos == val:
+                draw = True
+
+        if draw:
+            screen.append('#')
+        else:
+            screen.append(' ')
+
+    lines = []
+    for i in range(6):
+        line = screen[i*40: (i+1) * 40]
+        lines.append(''.join(line) + '\n')
+
+    return ''.join(lines)
+    # return ''.join(screen)
+
+
 if __name__ == "__main__":
     with open("input/day10.txt") as f:
         data = [line.strip() for line in f.readlines()]
 
     x_map = execute(data)
     print('---- Part One ----')
-    # pprint(x_map)
-    pprint(sum([num * x_map[num] for num in range(20, len(x_map), 40)]))
+    pprint(sum([num * x_map[num]
+           for num in range(20, len(x_map), 40)]))  # 17840
 
     print('---- Part Two ----')
-    print()
+    print(draw_CRT(x_map))  # EALGULPG
