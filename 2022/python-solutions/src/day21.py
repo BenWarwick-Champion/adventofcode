d39cfc9aa27a2f2d632d6ab146fc9d268ee7f025
Ben Warwick-Champion <ben.w-c@sky.com>
2022-12-21 21:47:03 +0000

    :poop: Cheesed day 21 part 2

--- 2022/python-solutions/src/day21.py
+++ 2022/python-solutions/src/day21.py
@@ -12,6 +12,30 @@ def calculate_p1(lines):
     return int(calc(monkeys, 'root'))
 
 
+def calculate_p2(lines):
+    monkeys = {}
+    for line in lines:
+        key, value = line.strip().split(': ')
+        monkeys[key] = value
+
+    left, _, right = monkeys['root'].split()
+    i = 3373767893000
+    equal = False
+
+    # monkeys['humn'] = f'{i}'
+    # print(i, int(calc(monkeys, left)) > int(calc(monkeys, right)))
+    # return int(calc(monkeys, left)), int(calc(monkeys, right))
+
+    while not equal:
+        i += 1
+        monkeys['humn'] = f'{i}'
+        if i % 1000 == 0:
+            print(int(calc(monkeys, left)), int(calc(monkeys, right)))
+        equal = int(calc(monkeys, left)) == int(calc(monkeys, right))
+
+    return i
+
+
 def calc(monkeys, monkey):
     if monkeys[monkey].isdecimal():
         return int(monkeys[monkey])
@@ -37,4 +61,4 @@ if __name__ == "__main__":
     pprint(calculate_p1(data))
 
     print('---- Part Two ----')
-    print()
+    pprint(calculate_p2(data))
