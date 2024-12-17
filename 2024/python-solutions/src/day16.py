64174e6c3776cb15f6d1d5f14333f105de5ecd35
Ben Warwick-Champion <ben.w-c@sky.com>
2024-12-17 10:13:34 +0000

    :christmas_tree: Day 16

--- 2024/python-solutions/src/day16.py
+++ 2024/python-solutions/src/day16.py
@@ -1,82 +1,57 @@
-# Advent of Code 2023
+# Advent of Code 2024
 # Day 16
 
-from collections import deque
+import heapq
+
+class Complex(complex):
+    __lt__=lambda s,o: (s.imag,s.real) < (o.imag,o.real)
+    __add__=lambda s,o: Complex(complex(s)+o)
 
 def parse_data(raw_data):
     maze = dict()
-    walls = set()
     for r, row in enumerate(raw_data):
         for c, char in enumerate(row):
-            maze[complex(r, c)] = char
-            if char == '#':
-                walls.add(complex(r, c))
+            maze[Complex(r, c)] = char
             if char == 'S':
-                start = complex(r, c)
+                start = Complex(r, c)
             if char == 'E':
-                end = complex(r, c)
-    return maze, walls, start, end
+                end = Complex(r, c)
+    return maze, start, end
 
-def print_maze(maze, path):
-    n_rows = int(max(p.real for p in maze) + 1)
-    n_cols = int(max(p.imag for p in maze))
-    for r in range(n_rows):
-        row = []
-        for c in range(n_cols):
-            pos = complex(r, c)
-            if pos in path:
-                row.append('O')
-            else:
-                row.append(maze[pos])
-        print(''.join(row))
+def dijkstra(maze, start, end):
+    queue = [(0, start, 1j, [start])]
+    visited = dict()
+    best = set()
+    low = float('inf')
 
-def find_neighbours(maze, walls, pos):
-    neighbours = []
-    for d in [1, -1, 1j, -1j]:
-        new_pos = pos + d
-        if new_pos in maze and new_pos not in walls:
-            neighbours.append(new_pos)
-    return neighbours
-
-def bfs(maze, walls, start, end):
-    queue = deque([(start, [])])
-    visited = set()
     while queue:
-        pos, path = queue.popleft()
+        cost, pos, direction, path = heapq.heappop(queue)
+        if cost > low: break
         if pos == end:
-            return path + [end]
-        if pos in visited:
-            continue
-        visited.add(pos)
-        for neighbour in find_neighbours(maze, walls, pos):
-            queue.append((neighbour, path + [pos]))
-    return -1
+            if low > cost: best.clear()
+            low = cost
+            best |= set(path)
+        visited[pos, direction] = cost
 
-def score(path):
-    direction = -1j
-    total = 0
-    for pos in path:
-        if pos + direction in path:
-            total += 1
-        else:
-            total += 1000
-            if pos + direction * 1j in path:
-                direction *= 1j
-            else:
-                direction *= -1j
-    return total
+        for d in map(Complex, (1, -1, 1j, -1j)):
+            if maze.get(pos + d, '#') == '#': continue
+            additional_cost = 1001 if d != direction else 1
+            if visited.get((pos + d, d), float('inf')) > cost + additional_cost:
+                heapq.heappush(queue, (cost + additional_cost, pos + d, d, path + [pos + d]))
+    return low, best
 
 def part_one(raw_data):
-    maze, walls, start, end = parse_data(raw_data)
-    shortest_path = bfs(maze, walls, start, end)
-    print_maze(maze, shortest_path)
-    return score(shortest_path)
+    maze, start, end = parse_data(raw_data)
+    cost, _ = dijkstra(maze, start, end)
+    return cost
 
 def part_two(raw_data):
-    return
+    maze, start, end = parse_data(raw_data)
+    _, best = dijkstra(maze, start, end)
+    return len(best)
 
 if __name__ == "__main__":
-    with open("input/_day16.txt") as f:
+    with open("input/day16.txt") as f:
         raw_data = f.readlines()
     
     print('---- Part One ----')
@@ -84,3 +59,4 @@ if __name__ == "__main__":
 
     print('---- Part Two ----')
     print(part_two(raw_data))
+
